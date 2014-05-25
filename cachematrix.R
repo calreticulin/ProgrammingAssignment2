2604e0491a5ad501d2f92741f1f4dd3a508632af
calreticulin <calreticulin@gmail.com>
2014-05-24 23:29:08 -0700

    submit

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,46 @@
-## Put comments here that give an overall description of what your
-## functions do
+## makeCacheMatrix: create a matrix that cache and invalidate cached inverse Matrix
+## cacheSolve: return cached solution whenever possible
 
-## Write a short comment describing this function
+# examples
+# > m <- matrix( rnorm(1000000,1,1),nrow=1000,ncol=1000)
+# > cm <- makeCacheMatrix(m)
+# > system.time(cacheSolve(cm))
+# user  system elapsed 
+# 0.832   0.004   0.836 
+# > system.time(cacheSolve(cm))
+# getting cached matrix
+# user  system elapsed 
+# 0.001   0.000   0.000 
+  
 
+## wrapper of a matrix object, keep inv as cached variable 
+## when the underlying matrix changes, cache is invalidated. 
 makeCacheMatrix <- function(x = matrix()) {
-
+  
+  inv <- NULL
+  set <- function(y){
+    x <<- y
+    inv <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(invM) inv <<- invM
+  getinverse <- function() inv
+  list(set=set, get=get, setinverse=setinverse, getinverse=getinverse)
 }
 
 
-## Write a short comment describing this function
+## In cacheSolve, it first check if the inverse if cached and return the cache
+## or it compute the inverse and set cache
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
+  cinv <- x$getinverse()
+  if(!is.null(cinv)) {
+    message("getting cached matrix")
+    return(cinv)
+  }
+  data <- x$get()
+  inv <- solve(data, ...)
+  x$setinverse(inv)
+  inv  
 }
